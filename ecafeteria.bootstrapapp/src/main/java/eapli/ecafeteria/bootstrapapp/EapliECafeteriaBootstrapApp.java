79bf68c4a89e8d36f91d927bd1876d69b2f69990
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2016-03-14 22:20:08 +0000

    created app settings created new project bootstrap app renamed artifacts

---
+++ ecafeteria.bootstrapapp/src/main/java/eapli/ecafeteria/bootstrapapp/EapliECafeteriaBootstrapApp.java
@@ -0,0 +1,13 @@
+package eapli.ecafeteria.bootstrapapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class EapliECafeteriaBootstrapApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ecafeteria.consoleapp/src/main/java/eapli/ecafeteria/consoleapp/AppSettings.java
@@ -0,0 +1,72 @@
+package eapli.ecafeteria.consoleapp;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A "global" (singleton) class with the application settings.
+ *
+ * @author Paulo Gandra Sousa
+ */
+public class AppSettings {
+
+	private final Properties	applicationProperties  = new Properties();
+	// FIXME use lazy holder idiom
+	private static AppSettings	theInstance;
+	private final static String	PROPERTIES_RESOURCE	   = "eapli/ecafeteria/ecafeteria.properties";
+	private final static String	REPOSITORY_FACTORY_KEY = "persistence.repositoryFactory";
+
+	public static AppSettings instance() {
+		if (theInstance == null) {
+			theInstance = new AppSettings();
+		}
+		return theInstance;
+	}
+
+	private AppSettings() {
+		loadProperties();
+	}
+
+	public Properties getApplicationProperties() {
+		return applicationProperties;
+	}
+
+	private void loadProperties() {
+		InputStream propertiesStream = null;
+		try {
+			// propertiesStream = new FileInputStream(PROPERTIES_FILENAME);
+			propertiesStream = AppSettings.class.getClassLoader().getResourceAsStream(PROPERTIES_RESOURCE);
+			if (propertiesStream != null) {
+				applicationProperties.load(propertiesStream);
+			} else {
+				throw new FileNotFoundException(
+				        "property file '" + PROPERTIES_RESOURCE + "' not found in the classpath");
+			}
+		} catch (IOException exio) {
+			setDefaultProperties();
+
+			Logger.getLogger(AppSettings.class.getName()).log(Level.SEVERE, null, exio);
+		} finally {
+			if (propertiesStream != null) {
+				try {
+					propertiesStream.close();
+				} catch (IOException ex) {
+					Logger.getLogger(AppSettings.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+		}
+	}
+
+	private void setDefaultProperties() {
+		applicationProperties.setProperty(REPOSITORY_FACTORY_KEY,
+		        "eapli.ecafeteria.persistence.jpa.JpaRepositoryFactory");
+	}
+
+	public String getRepositoryFactory() {
+		return applicationProperties.getProperty(REPOSITORY_FACTORY_KEY);
+	}
+}
