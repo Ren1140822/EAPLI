7fe4923b1165dd8d045f142024535529f6d38cac
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2016-03-22 21:59:24 +0000

    minor refactoring

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/users/AuthenticationService.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/users/AuthenticationService.java
@@ -4,26 +4,28 @@ import eapli.ecafeteria.persistence.PersistenceContext;
 
 public class AuthenticationService {
 
-    /**
-     * Checks if a user can be authenticated with the username/password pair
-     *
-     * @param username
-     * @param pass
-     * @return the authenticated user or null otherwise
-     */
-    public Session authenticate(Username username, Password pass) throws InvalidUserException, InvalidPasswordException {
-        User user = retrieveUser(username);
-        if (null==user)
-                throw new InvalidUserException("Invalid User");
-        user.passwordMatches(pass);
-        return createSessionForUser(user);
-    }
+	/**
+	 * Checks if a user can be authenticated with the username/password pair
+	 *
+	 * @param username
+	 * @param pass
+	 * @return the authenticated user or null otherwise
+	 */
+	public Session authenticate(Username username, Password pass)
+	        throws InvalidUserException, InvalidPasswordException {
+		final User user = retrieveUser(username);
+		if (null == user) {
+			throw new InvalidUserException("Invalid User");
+		}
+		user.passwordMatches(pass);
+		return createSessionForUser(user);
+	}
 
-    private Session createSessionForUser(User user) {
-        return new Session(user);
-    }
+	private Session createSessionForUser(User user) {
+		return new Session(user);
+	}
 
-    public User retrieveUser(Username userName) {
-        return PersistenceContext.repositories().users().findById(userName);
-    }
+	private User retrieveUser(Username userName) {
+		return PersistenceContext.repositories().users().findById(userName);
+	}
 }
