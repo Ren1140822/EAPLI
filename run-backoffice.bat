dc46e365e28a394dfb140134af567b97d4c4ac70
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2016-03-28 22:34:32 +0100

    fixed scripts

--- run-backoffice.bat
+++ run-backoffice.bat
@@ -1,6 +1,6 @@
-REM set the class path, eg.
-SET EAPLI_CP=backoffice.consoleapp\target\ecafeteria.backoffice.consoleapp-1.0-SNAPSHOT.jar;consoleapp.common\target\consoleapp.common-0.0.1-SNAPSHOT.jar;ecafeteria.bootstrapapp\target\ecafeteria.bootstrapapp-0.0.1-SNAPSHOT.jar;ecafeteria.core\target\ecafeteria.core-1.0-SNAPSHOT.jar;framework\target\framework-1.0-SNAPSHOT.jar;util\target\util-1.0-SNAPSHOT.jar; 
-REM SET EAPLI_CP=%EAPLI_CP%;LIBS\eclipselink\eclipselink-2.3.2.jar;LIBS\eclipselink\javax.persistence-2.0.3.jar;LIBS\eclipselink\org.eclipse.persistence.jpa.jpql_1.0.1.jar;LIBS\h2-1.3.171.jar;LIBS\jfreechart-1.0.14.jar;LIBS\jcommon-1.0.17.jar
+REM set the class path,
+REM assumes the build was executed with maven copy-dependencies
+SET ECAFETERIA_CP=backoffice.consoleapp\target\ecafeteria.backoffice.consoleapp-1.0-SNAPSHOT.jar;backoffice.consoleapp\target\dependency\*;
 
 REM call the java VM, e.g, 
-java -cp %EAPLI_CP% eapli.ecafeteria.backoffice.consoleapp.ECafeteriaBackoffice
+java -cp %ECAFETERIA_CP% eapli.ecafeteria.backoffice.consoleapp.ECafeteriaBackoffice
