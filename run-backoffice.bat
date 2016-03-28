462eaaaef3167192497529259ab1a7818693b181
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2016-03-28 20:15:05 +0100

    adjusted script to use the compiled jars needs to add dependency libraries

--- run-backoffice.bat
+++ run-backoffice.bat
@@ -1,6 +1,6 @@
 REM set the class path, eg.
-REM SET CP = eapli.ecafeteria.consoleapp\bin;eapli.ecafeteria.core\bin;eapli.framework\bin;eapli.util\bin;
-REM SET CP = %CP;LIBS\eclipselink\eclipselink-2.3.2.jar;LIBS\eclipselink\javax.persistence-2.0.3.jar;LIBS\eclipselink\org.eclipse.persistence.jpa.jpql_1.0.1.jar;LIBS\h2-1.3.171.jar;LIBS\jfreechart-1.0.14.jar;LIBS\jcommon-1.0.17.jar
+SET EAPLI_CP=backoffice.consoleapp\target\ecafeteria.backoffice.consoleapp-1.0-SNAPSHOT.jar;consoleapp.common\target\consoleapp.common-0.0.1-SNAPSHOT.jar;ecafeteria.bootstrapapp\target\ecafeteria.bootstrapapp-0.0.1-SNAPSHOT.jar;ecafeteria.core\target\ecafeteria.core-1.0-SNAPSHOT.jar;framework\target\framework-1.0-SNAPSHOT.jar;util\target\util-1.0-SNAPSHOT.jar; 
+REM SET EAPLI_CP=%EAPLI_CP%;LIBS\eclipselink\eclipselink-2.3.2.jar;LIBS\eclipselink\javax.persistence-2.0.3.jar;LIBS\eclipselink\org.eclipse.persistence.jpa.jpql_1.0.1.jar;LIBS\h2-1.3.171.jar;LIBS\jfreechart-1.0.14.jar;LIBS\jcommon-1.0.17.jar
 
 REM call the java VM, e.g, 
-REM java -cp %CP eapli.ecafeteria.EapliEcafeteriaConsoleApp
+java -cp %EAPLI_CP% eapli.ecafeteria.backoffice.consoleapp.ECafeteriaBackoffice
