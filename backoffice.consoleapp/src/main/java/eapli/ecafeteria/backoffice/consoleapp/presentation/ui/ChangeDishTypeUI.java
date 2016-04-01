549bbf4e6940671fdd67faf59c652e944934566c
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2016-04-01 22:23:14 +0100

    fix initialization of locla variable

--- backoffice.consoleapp/src/main/java/eapli/ecafeteria/backoffice/consoleapp/presentation/ui/ChangeDishTypeUI.java
+++ backoffice.consoleapp/src/main/java/eapli/ecafeteria/backoffice/consoleapp/presentation/ui/ChangeDishTypeUI.java
@@ -1,9 +1,11 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
  */
 package eapli.ecafeteria.backoffice.consoleapp.presentation.ui;
+
+import java.util.Iterator;
 
 import eapli.ecafeteria.application.ChangeDishTypeController;
 import eapli.ecafeteria.application.ListDishTypeController;
@@ -11,51 +13,51 @@ import eapli.ecafeteria.domain.DishType;
 import eapli.framework.application.Controller;
 import eapli.framework.presentation.console.AbstractUI;
 import eapli.util.Console;
-import java.util.Iterator;
 
 /**
  *
  * @author Nuno
  */
-public class ChangeDishTypeUI extends AbstractUI{
-    
+public class ChangeDishTypeUI extends AbstractUI {
+
     private final ChangeDishTypeController theController = new ChangeDishTypeController();
 
     @Override
     protected Controller controller() {
-        return theController;
+        return this.theController;
     }
 
     @Override
     protected boolean doShow() {
-           //TODO a UI class should only interact with one controller
-           Iterable<DishType> allDishTypes = new ListDishTypeController().listDishTypes();
-           Iterator<DishType> dishTypeIterator = allDishTypes.iterator();
-           //TODO we should not call a new UI from another to do this kind of things. there should be a UI widget reused in both UIs
-           Boolean res = new ListDishTypeUI().doShowIterable(allDishTypes);
-           
-           int dishTypeKey = Console.readInteger("Select dish type to change");
-           int n = 0;
-           //iterators do not implement random access, sequential access required to reach the object selected by user
-           DishType updtDishType;
-           while (dishTypeIterator.hasNext() && n != dishTypeKey) {
-                updtDishType = dishTypeIterator.next();
-                n++;
-           }
+        // TODO a UI class should only interact with one controller
+        final Iterable<DishType> allDishTypes = new ListDishTypeController().listDishTypes();
+        final Iterator<DishType> dishTypeIterator = allDishTypes.iterator();
+        new ListDishTypeUI().doShowIterable(allDishTypes);
 
-           if (dishTypeKey == n) { //DishType selected by user exists
-               String newDescription = Console.readLine("Enter new description for " + updtDishType.description() + ": ");
-               updtDishType.changeDescriptionTo(newDescription);
-               theController.changeDishType(updtDishType);
-           } else { //DishType selected by user does not exist
-               System.out.print("Invalid dish type. Select one of the items in the list.");
-           }
-               
+        final int dishTypeKey = Console.readInteger("Select dish type to change");
+        int n = 0;
+        // iterators do not implement random access, sequential access required
+        // to reach the object selected by user
+        DishType updtDishType = null;
+        while (dishTypeIterator.hasNext() && n != dishTypeKey) {
+            updtDishType = dishTypeIterator.next();
+            n++;
+        }
+
+        if (dishTypeKey == n) { // DishType selected by user exists
+            final String newDescription = Console
+                    .readLine("Enter new description for " + updtDishType.description() + ": ");
+            updtDishType.changeDescriptionTo(newDescription);
+            this.theController.changeDishType(updtDishType);
+        } else { // DishType selected by user does not exist
+            System.out.print("Invalid dish type. Select one of the items in the list.");
+        }
+
         return true;
     }
 
     @Override
     public String headline() {
-        return "Change Dish Type description";  
+        return "Change Dish Type description";
     }
 }
