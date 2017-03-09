73866e8fb246cce79d8b6e22873cc60a290c67f4
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2017-03-09 16:35:07 +0000

    added password business rules and unit test

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/authz/Password.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/authz/Password.java
@@ -3,12 +3,20 @@
  */
 package eapli.ecafeteria.domain.authz;
 
-import eapli.framework.domain.ValueObject;
-import eapli.util.Strings;
 import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import eapli.framework.domain.ValueObject;
+import eapli.util.Strings;
+
 /**
+ * a password of a user.
+ *
+ * passwords must be at least 6 characters long and have at least one digit and
+ * one capital letter
+ *
  * TODO passwords should never be stored in plain format
  *
  * @author Paulo Gandra Sousa
@@ -16,41 +24,63 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Password implements ValueObject, Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
-    private String password;
+    @Column(name = "pass")
+    private String thePassword;
 
     protected Password() {
-        // for ORM only
+	// for ORM only
     }
 
     public Password(String password) {
-        if (Strings.isNullOrEmpty(password)) {
-            throw new IllegalStateException();
-        }
-        this.password = password;
+	if (!meetsMinimumRequirements(password)) {
+	    throw new IllegalStateException();
+	}
+	thePassword = password;
+    }
+
+    private boolean meetsMinimumRequirements(String password) {
+	// sanity check
+	if (Strings.isNullOrEmpty(password)) {
+	    return false;
+	}
+
+	// at least 6 characters long
+	if (password.length() < 6) {
+	    return false;
+	}
+
+	// at least one digit
+	if (!Strings.containsDigit(password)) {
+	    return false;
+	}
+
+	// at least one capital letter
+	if (!Strings.containsCapital(password)) {
+	    return false;
+	}
+
+	return true;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Password)) {
-            return false;
-        }
+	if (this == o) {
+	    return true;
+	}
+	if (!(o instanceof Password)) {
+	    return false;
+	}
 
-        final Password password1 = (Password) o;
+	final Password password1 = (Password) o;
 
-        return this.password.equals(password1.password);
+	return thePassword.equals(password1.thePassword);
 
     }
 
     @Override
     public int hashCode() {
-        return this.password.hashCode();
+	return thePassword.hashCode();
     }
 
     /**
@@ -59,20 +89,20 @@ public class Password implements ValueObject, Serializable {
      * @return how strong a password is
      */
     public PasswordStrength strength() {
-        PasswordStrength passwordStrength = PasswordStrength.Weak;
-        if (3 > this.password.length()) {
-            passwordStrength = PasswordStrength.Weak;
-        }
-        return passwordStrength;
-        // TODO implement the rest of the method
+	PasswordStrength passwordStrength = PasswordStrength.Weak;
+	if (3 > thePassword.length()) {
+	    passwordStrength = PasswordStrength.Weak;
+	}
+	return passwordStrength;
+	// TODO implement the rest of the method
     }
 
     @Override
     public String toString() {
-        return this.password;
+	return thePassword;
     }
 
     public enum PasswordStrength {
-        Weak, Good, Excelent,
+	Weak, Good, Excelent,
     }
 }
