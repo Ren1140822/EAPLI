54463404ad933559d753af9dc4833f9e1984c2f5
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2017-03-23 15:48:32 +0000

    avoid exception if user enters "bad" password

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/authz/LoginController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/authz/LoginController.java
@@ -22,11 +22,15 @@ public class LoginController implements Controller {
      * @param password
      */
     public boolean login(String userName, String password) {
-        final Optional<UserSession> newSession = this.authenticationService.authenticate(new Username(userName),
-                new Password(password));
-        if (newSession.isPresent()) {
-            Application.session().setSession(newSession.get());
+        try {
+            final Optional<UserSession> newSession = authenticationService.authenticate(new Username(userName),
+                    new Password(password));
+            if (newSession.isPresent()) {
+                Application.session().setSession(newSession.get());
+            }
+            return newSession.isPresent();
+        } catch (final IllegalStateException e) {
+            return false;
         }
-        return newSession.isPresent();
     }
 }
