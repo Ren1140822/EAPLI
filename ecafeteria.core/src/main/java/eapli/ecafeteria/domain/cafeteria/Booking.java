5bd2eaff564f1fc6a65397875ec73d782a2ba025
DESKTOP-7NNSDNL\PC <1150838@isep.ipp.pt>
2017-04-25 15:24:48 +0100

    add basic Booking class and unit tests. Added Empty Meal Class

---
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteria/Booking.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.domain.cafeteria;
+
+import eapli.ecafeteria.domain.meals.Meal;
+import eapli.framework.domain.AggregateRoot;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+
+/**
+ *
+ * @author Nuno Pinto [1150838@isep.ipp.pt] Henrique Oliveira [1150738@isep.ipp.pt]
+ */
+@Entity
+public class Booking implements Serializable  {
+    
+    @Version
+    private Long version;
+    
+    @Id
+    @GeneratedValue
+    private Long pk;
+    
+    @OneToMany(cascade = CascadeType.MERGE)
+    private CafeteriaUser user;
+    
+    @OneToMany(cascade = CascadeType.MERGE)
+    private Meal meal;
+    
+    public Booking(){
+        // for ORM
+    }
+    
+    public Booking(CafeteriaUser user, Meal meal){
+        if (user == null || meal == null ) {
+            throw new IllegalStateException();
+        }
+        this.user=user;
+        this.meal=meal;
+    }
+
+
+    
+    
+    
+}
