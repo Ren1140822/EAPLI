fa2aac8dacb88787cbe45c3ac2c07d724df71bd6
Meu Nome <eu@exemplo.com>
2017-04-27 16:12:55 +0100

    [UC-MM-03-A][Issue #8] RegisterAllergenController finished

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/meals/RegisterAllergenController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/meals/RegisterAllergenController.java
@@ -1,10 +1,8 @@
 package eapli.ecafeteria.application.meals;
 
-import eapli.ecafeteria.Application;
-import eapli.ecafeteria.domain.authz.ActionRight;
+import eapli.ecafeteria.domain.meals.Allergen;
+import eapli.ecafeteria.persistence.AllergenRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
-import eapli.framework.persistence.DataConcurrencyException;
-import eapli.framework.persistence.DataIntegrityViolationException;
 
 /**
  *
@@ -12,20 +10,10 @@ import eapli.framework.persistence.DataIntegrityViolationException;
  */
 public class RegisterAllergenController {
     
-    private final ListAllergenService svcAllergen = new ListAllergenService();
+    private final ListAllergensService svcAllergen = new ListAllergensService();
     private final AllergenRepository allergenRepository = PersistenceContext.repositories().allergens();
 
     public Iterable<Allergen> allAllergen() {
-        return this.svcAllergen.allAllergen();
-    }
-
-    public void changeAllergenState(Allergen allergen) throws DataConcurrencyException, DataIntegrityViolationException {
-        Application.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_MENUS);
-        if (allergen == null) {
-            throw new IllegalArgumentException();
-        }
-        allergen.toogleState();
-
-        Allergen ret = this.AllergenRepository.save(allergen);
+        return this.svcAllergen.allAllergens();
     }
 }
