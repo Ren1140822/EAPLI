2f5ea7425bf4c39a09480da6d642c63afa04ec54
João Cardoso <1150943@isep.ipp.pt>
2017-04-28 17:49:37 +0100

    UC-MM-03-A [Issue #8] Created bootstrap of allergens

--- ecafeteria.bootstrap.core/src/main/java/eapli/ecafeteria/bootstrapers/AllergenBootstraper.java
+++ ecafeteria.bootstrap.core/src/main/java/eapli/ecafeteria/bootstrapers/AllergenBootstraper.java
@@ -1,11 +1,9 @@
 package eapli.ecafeteria.bootstrapers;
 
-import eapli.ecafeteria.application.meals.RegisterDishController;
-import eapli.ecafeteria.domain.meals.DishType;
+import eapli.ecafeteria.application.meals.AddAllergensService;
 import eapli.ecafeteria.persistence.AllergenRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
 import eapli.framework.actions.Action;
-import eapli.framework.domain.Designation;
 import eapli.framework.persistence.DataConcurrencyException;
 import eapli.framework.persistence.DataIntegrityViolationException;
 
@@ -17,21 +15,34 @@ import java.util.logging.Logger;
 public class AllergenBootstraper implements Action {
     @Override
     public boolean execute() {
-        /*/AllergenRepository repo = PersistenceContext.repositories().allergens();
-        register()/*/
-        return true;
+        AllergenRepository repo = PersistenceContext.repositories().allergens();
+        register("Cereals containing gluten","wheat, rye, barley, oats or their hybridised strains, and products thereof");
+        register("Crustaceans and products thereof","");
+        register("Eggs and products thereof","");
+        register("Fish and products thereof","");
+        register("Peanuts and products thereof","");
+        register("Soybeans and products thereof","");
+        register("Milk and products thereof","Milk and products thereof (including lactose)");
+        register("Nuts","almonds, hazelnuts, walnuts, cashews, pecan nuts, Brazil nuts, pistachio nuts, macadamia, and products thereof, except for nuts used for making alcoholic distillates including ethyl alcohol of agricultural origin");
+        register("Celery and products thereof","");
+        register("Mustard and products thereof","");
+        register("Sesame seeds and products thereof","");
+        register("Sulphur dioxide and sulphites","Sulphur dioxide and sulphites at concentrations of more than 10 mg/kg or 10 mg/litre");
+        register("Lupin and products thereof","");
+        register("Molluscs and products thereof","");
+        return false;
     }
 
 
-    private void register(Designation name, String description) {
-        /*/final RegisterAllergenController controller = new RegisterAllergenController();
+    private void register(String name, String description) {
+        final AddAllergensService svc = new AddAllergensService();
         try {
-            controller.registerAllergen(name, description);
+            svc.registerAllergen(name, description);
         } catch (final DataIntegrityViolationException | DataConcurrencyException e) {
             // ignoring exception. assuming it is just a primary key violation
             // due to the tentative of inserting a duplicated user
             Logger.getLogger(ECafeteriaBootstraper.class.getSimpleName())
                     .info("EAPLI-DI001: bootstrapping existing record");
-        }/*/
+        }
     }
 }
