e2d9c5d315910500f81eec2ea7508ccf94f56106
Diogo Alexandre Vasconcelos Santos <1150451@isep.ipp.pt>
2017-05-04 13:29:03 +0100

    [UC-KM-03][Issue #14] Fixes in MealsPreparedBootstrapper and JPAMealRepository

--- framework/src/main/java/eapli/framework/domain/TimePeriod2.java
+++ framework/src/main/java/eapli/framework/domain/TimePeriod2.java
@@ -4,6 +4,7 @@
 package eapli.framework.domain;
 
 import java.util.Calendar;
+import java.util.Objects;
 import javax.persistence.Embeddable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -45,4 +46,35 @@ public class TimePeriod2 {
     public Calendar end() {
 	return this.start;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.start);
+        hash = 17 * hash + Objects.hashCode(this.end);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TimePeriod2 other = (TimePeriod2) obj;
+        if (!Objects.equals(this.start, other.start)) {
+            return false;
+        }
+        if (!Objects.equals(this.end, other.end)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
