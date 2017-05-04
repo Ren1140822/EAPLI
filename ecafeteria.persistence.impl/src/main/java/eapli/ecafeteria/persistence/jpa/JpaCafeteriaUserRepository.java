8ac3f7f28b846494d53018c751168a312858e5ef
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2017-05-04 14:12:58 +0100

    use parameters for queries instead of string concatenation

--- ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaCafeteriaUserRepository.java
+++ ecafeteria.persistence.impl/src/main/java/eapli/ecafeteria/persistence/jpa/JpaCafeteriaUserRepository.java
@@ -7,27 +7,32 @@ import eapli.ecafeteria.domain.cafeteria.MecanographicNumber;
 import eapli.ecafeteria.persistence.CafeteriaUserRepository;
 import eapli.framework.persistence.repositories.TransactionalContext;
 import eapli.framework.persistence.repositories.impl.jpa.JpaAutoTxRepository;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Jorge Santos ajs@isep.ipp.pt 02/04/2016
  */
 class JpaCafeteriaUserRepository extends JpaAutoTxRepository<CafeteriaUser, MecanographicNumber>
-	implements CafeteriaUserRepository {
+        implements CafeteriaUserRepository {
 
     public JpaCafeteriaUserRepository(TransactionalContext autoTx) {
-	super(Application.settings().getPersistenceUnitName(), autoTx);
+        super(Application.settings().getPersistenceUnitName(), autoTx);
     }
 
     @Override
     public CafeteriaUser findByUsername(Username name) {
-	// FIXME use parameters instead of string concatenation
-	return repo.matchOne("e.systemUser.username.name='" + name + "'");
+        // FIXME use parameters instead of string concatenation
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        return repo.matchOne("e.systemUser.username.name=:name", params);
     }
 
     @Override
     public CafeteriaUser findByMecanographicNumber(MecanographicNumber number) {
-	// FIXME use parameters instead of string concatenation
-	return repo.matchOne("e.mecanographicNumber.number='" + number + "'");
+        Map<String, Object> params = new HashMap<>();
+        params.put("number", number);
+        return repo.matchOne("e.mecanographicNumber.number=:number", params);
     }
 }
