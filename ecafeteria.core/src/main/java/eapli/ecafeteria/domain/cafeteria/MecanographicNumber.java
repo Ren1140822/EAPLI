291ba3333ecba72f26b90cc0494cb372241d632c
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2017-05-04 12:08:21 +0100

    fix for framewrok changes

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteria/MecanographicNumber.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteria/MecanographicNumber.java
@@ -5,7 +5,7 @@
  */
 package eapli.ecafeteria.domain.cafeteria;
 
-import eapli.framework.domain.ValueObject;
+import eapli.framework.domain.ddd.ValueObject;
 import eapli.util.Strings;
 import java.io.Serializable;
 import javax.persistence.Embeddable;
@@ -22,39 +22,39 @@ public class MecanographicNumber implements ValueObject, Serializable {
     private String number;
 
     public MecanographicNumber(String mecanographicNumber) {
-	if (Strings.isNullOrEmpty(mecanographicNumber)) {
-	    throw new IllegalStateException("Mecanographic Number should neither be null nor empty");
-	}
-	// FIXME validate invariants, i.e., mechanographic number regular
-	// expression
-	this.number = mecanographicNumber;
+        if (Strings.isNullOrEmpty(mecanographicNumber)) {
+            throw new IllegalStateException("Mecanographic Number should neither be null nor empty");
+        }
+        // FIXME validate invariants, i.e., mechanographic number regular
+        // expression
+        this.number = mecanographicNumber;
     }
 
     protected MecanographicNumber() {
-	// for ORM
+        // for ORM
     }
 
     @Override
     public boolean equals(Object o) {
-	if (this == o) {
-	    return true;
-	}
-	if (!(o instanceof MecanographicNumber)) {
-	    return false;
-	}
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MecanographicNumber)) {
+            return false;
+        }
 
-	final MecanographicNumber that = (MecanographicNumber) o;
+        final MecanographicNumber that = (MecanographicNumber) o;
 
-	return this.number.equals(that.number);
+        return this.number.equals(that.number);
     }
 
     @Override
     public int hashCode() {
-	return this.number.hashCode();
+        return this.number.hashCode();
     }
 
     @Override
     public String toString() {
-	return this.number;
+        return this.number;
     }
 }
--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/Dish.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/meals/Dish.java
@@ -1,8 +1,8 @@
 package eapli.ecafeteria.domain.meals;
 
-import eapli.framework.domain.AggregateRoot;
 import eapli.framework.domain.Designation;
 import eapli.framework.domain.Money;
+import eapli.framework.domain.ddd.AggregateRoot;
 import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
@@ -34,90 +34,90 @@ public class Dish implements AggregateRoot<Designation>, Serializable {
     private boolean active;
 
     public Dish(final DishType dishType, final Designation name, final NutricionalInfo nutricionalInfo, Money price) {
-	if (dishType == null || name == null || nutricionalInfo == null) {
-	    throw new IllegalStateException();
-	}
+        if (dishType == null || name == null || nutricionalInfo == null) {
+            throw new IllegalStateException();
+        }
 
-	this.dishType = dishType;
-	this.name = name;
-	this.nutricionalInfo = nutricionalInfo;
-	this.setPrice(price);
-	this.active = true;
+        this.dishType = dishType;
+        this.name = name;
+        this.nutricionalInfo = nutricionalInfo;
+        this.setPrice(price);
+        this.active = true;
     }
 
     public Dish(final DishType dishType, final Designation name, Money price) {
-	if (dishType == null || name == null || price == null) {
-	    throw new IllegalStateException();
-	}
+        if (dishType == null || name == null || price == null) {
+            throw new IllegalStateException();
+        }
 
-	this.dishType = dishType;
-	this.name = name;
-	this.nutricionalInfo = null;
-	this.price = price;
-	this.active = true;
+        this.dishType = dishType;
+        this.name = name;
+        this.nutricionalInfo = null;
+        this.price = price;
+        this.active = true;
     }
 
     protected Dish() {
-	// for ORM only
+        // for ORM only
     }
 
     @Override
     public boolean equals(Object o) {
-	if (this == o) {
-	    return true;
-	}
-	if (!(o instanceof Dish)) {
-	    return false;
-	}
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Dish)) {
+            return false;
+        }
 
-	final Dish other = (Dish) o;
-	return id().equals(other.id());
+        final Dish other = (Dish) o;
+        return id().equals(other.id());
     }
 
     @Override
     public int hashCode() {
-	return this.name.hashCode();
+        return this.name.hashCode();
     }
 
     @Override
     public boolean sameAs(Object other) {
-	if (!(other instanceof Dish)) {
-	    return false;
-	}
+        if (!(other instanceof Dish)) {
+            return false;
+        }
 
-	final Dish that = (Dish) other;
-	if (this == that) {
-	    return true;
-	}
+        final Dish that = (Dish) other;
+        if (this == that) {
+            return true;
+        }
 
-	return id().equals(that.id()) && dishType.equals(that.dishType) && nutricionalInfo.equals(that.nutricionalInfo)
-		&& price.equals(that.price) && active == that.active;
+        return id().equals(that.id()) && dishType.equals(that.dishType) && nutricionalInfo.equals(that.nutricionalInfo)
+                && price.equals(that.price) && active == that.active;
     }
 
     @Override
     public boolean is(Designation id) {
-	return id().equals(id);
+        return id().equals(id);
     }
 
     public DishType dishType() {
-	return this.dishType;
+        return this.dishType;
     }
 
     @Override
     public Designation id() {
-	return this.name;
+        return this.name;
     }
 
     public NutricionalInfo nutricionalInfo() {
-	return this.nutricionalInfo;
+        return this.nutricionalInfo;
     }
 
     public Designation name() {
-	return this.name;
+        return this.name;
     }
 
     public Money currentPrice() {
-	return this.price;
+        return this.price;
     }
 
     /**
@@ -125,7 +125,7 @@ public class Dish implements AggregateRoot<Designation>, Serializable {
      * @return true or false whether is or not active
      */
     public boolean isActive() {
-	return this.active;
+        return this.active;
     }
 
     /**
@@ -135,8 +135,8 @@ public class Dish implements AggregateRoot<Designation>, Serializable {
      * @return whether the dish is active or not
      */
     public boolean toogleState() {
-	this.active = !this.active;
-	return isActive();
+        this.active = !this.active;
+        return isActive();
     }
 
     /**
@@ -145,22 +145,22 @@ public class Dish implements AggregateRoot<Designation>, Serializable {
      * @param newNutricionalInfo
      */
     public void changeNutricionalInfoTo(NutricionalInfo newNutricionalInfo) {
-	if (newNutricionalInfo == null) {
-	    throw new IllegalArgumentException();
-	}
-	this.nutricionalInfo = newNutricionalInfo;
+        if (newNutricionalInfo == null) {
+            throw new IllegalArgumentException();
+        }
+        this.nutricionalInfo = newNutricionalInfo;
     }
 
     public void changePriceTo(Money newPrice) {
-	// TODO extra business logic associated with changing the price of a
-	// dish, e.g., save price history
-	setPrice(newPrice);
+        // TODO extra business logic associated with changing the price of a
+        // dish, e.g., save price history
+        setPrice(newPrice);
     }
 
     private void setPrice(Money price) {
-	if (price == null || price.lessThanOrEqual(Money.euros(0))) {
-	    throw new IllegalArgumentException();
-	}
-	this.price = price;
+        if (price == null || price.lessThanOrEqual(Money.euros(0))) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
     }
 }
