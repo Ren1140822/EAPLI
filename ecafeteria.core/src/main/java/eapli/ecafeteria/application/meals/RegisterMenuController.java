f782aba8e48b675d7cfe2f8e0fc2df0642fb69e7
Pyska Ratzinger <ricardojorge512@hotmail.com>
2017-05-05 17:27:00 +0100

    [Fixes Issue #4] Menu agora já tem uma unidade orgânica

--- ecafeteria.core/src/main/java/eapli/ecafeteria/application/meals/RegisterMenuController.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/application/meals/RegisterMenuController.java
@@ -1,7 +1,9 @@
 package eapli.ecafeteria.application.meals;
 
 import eapli.ecafeteria.Application;
+import eapli.ecafeteria.application.cafeteria.ListOrganicUnitsController;
 import eapli.ecafeteria.domain.authz.ActionRight;
+import eapli.ecafeteria.domain.cafeteria.OrganicUnit;
 import eapli.ecafeteria.domain.meals.*;
 import eapli.ecafeteria.persistence.MenuRepository;
 import eapli.ecafeteria.persistence.PersistenceContext;
@@ -21,13 +23,14 @@ public class RegisterMenuController implements Controller {
     private final MenuRepository repository = PersistenceContext.repositories().menus();
     private final ListDishService dishService = new ListDishService();
     private final ListMealTypeService mealTypeService = new ListMealTypeService();
+    private final ListOrganicUnitsService organicUnitsService = new ListOrganicUnitsService();
 
-    public Menu registerMenu(Dish dish, MealType mealType, TimePeriod2 period, Calendar date)
+    public Menu registerMenu(Dish dish, MealType mealType, OrganicUnit organicUnit, TimePeriod2 period, Calendar date)
             throws DataIntegrityViolationException, DataConcurrencyException {
         Application.ensurePermissionOfLoggedInUser(ActionRight.MANAGE_MENUS);
 
         final Meal meal = new Meal(dish, mealType, date);
-        final Menu newMenu = new Menu(period);
+        final Menu newMenu = new Menu(period, organicUnit);
         newMenu.addMeal(meal);
 
         Menu menu = repository.save(newMenu);
@@ -40,5 +43,9 @@ public class RegisterMenuController implements Controller {
 
     public Iterable<MealType.MealTypes> allMealTypes(){
         return mealTypeService.allMealTypes();
+    }
+
+    public Iterable<OrganicUnit> allOrganicUnits() {
+        return organicUnitsService.listOrganicUnits();
     }
 }
