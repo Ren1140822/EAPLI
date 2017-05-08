b0b043c693aeb00a2b3e7877e0270bf60e7bf6ca
Tiago Correia <1151031@isep.ipp.pt>
2017-05-08 15:17:41 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/git/tutorial/Cat.java

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteria/cashregister/CashRegisterState.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteria/cashregister/CashRegisterState.java
@@ -1,10 +1,8 @@
 package eapli.ecafeteria.domain.cafeteria.cashregister;
 
 /**
- *
  * @author Eric Amaral - 1141570@isep.ipp.pt
  * @author Tiago Correia - 1151031@isep.ipp.pt
- *
  */
 public enum CashRegisterState {
     OPENED, CLOSED;
