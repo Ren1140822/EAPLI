30ef497b3138bf1ad8c67fd25fa0c16eccf0a665
Manuel Joaquim Carvalho de Meireles <1150532@isep.ipp.pt>
2017-05-20 19:14:24 +0100

    [UC-MB-13][UC-MB-14] I've changed the UI of the User application in order to show both user's balance and next booking in all menus.

--- user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/presentation/CafeteriaUserBaseUI.java
+++ user.consoleapp/src/main/java/eapli/ecafeteria/user/consoleapp/presentation/CafeteriaUserBaseUI.java
@@ -9,6 +9,11 @@ import eapli.ecafeteria.Application;
 import eapli.ecafeteria.application.CafeteriaUserBaseController;
 import eapli.ecafeteria.domain.booking.Booking;
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.HorizontalMenuRenderer;
+import eapli.framework.presentation.console.Menu;
+import eapli.framework.presentation.console.MenuRenderer;
+import eapli.framework.presentation.console.VerticalMenuRenderer;
+import eapli.framework.presentation.console.VerticalSeparator;
 import eapli.util.DateTime;
 
 /**
@@ -19,18 +24,22 @@ public abstract class CafeteriaUserBaseUI extends AbstractUI {
 
     protected abstract CafeteriaUserBaseController controller();
 
-    public String showBalance() {
-        return "CURRENT BALANCE OF YOUR USERCARD: " + "\n" + controller().balance().toString();
+    public String id() {
+        return controller().id().toString();
     }
 
-    public String showNextBooking() {
+    public String balance() {
+        return "CURRENT BALANCE OF YOUR USERCARD: " + controller().balance().toString();
+    }
+
+    public String nextBooking() {
         Booking nextBooking = this.controller().nextBooking();
         String booking;
 
         if (nextBooking == null) {
             booking = "There are no bookings.";
         } else {
-            booking = String.format("\n%-15s%-10s%-30s\n", DateTime.format(nextBooking.meal().getDate()),
+            booking = String.format("%-11s%-7s%-30s", DateTime.format(nextBooking.meal().getDate()),
                     nextBooking.meal().mealType().mealType(),
                     nextBooking.meal().dish().name());
         }
@@ -39,15 +48,51 @@ public abstract class CafeteriaUserBaseUI extends AbstractUI {
 
     @Override
     public String headline() {
-        return "eCAFETERIA [@" + Application.session().session().authenticatedUser().id() + "]   " + "\n\n" + showBalance() + "\n\n"
-                + showNextBooking();
+        StringBuilder headline = new StringBuilder();
+        headline.append(" eCAFETERIA [@");
+        headline.append(id());
+        headline.append("] ");
+        int start = headline.length() + 3;
+        int end = BORDER.length();
+        if (start < end) {
+            headline.append(BORDER.substring(start, end));
+        }
+        headline.append(System.lineSeparator());
+        headline.append(System.lineSeparator());
+        headline.append(balance());
+        headline.append(System.lineSeparator());
+        headline.append(System.lineSeparator());
+        headline.append(nextBooking());
+        headline.append(System.lineSeparator());
+        return headline.toString();
     }
 
     @Override
     protected void drawFormTitle(String title) {
-        // drawFormBorder();
         final String titleBorder = BORDER.substring(0, 2) + " " + title;
         System.out.println(titleBorder);
         drawFormBorder();
     }
+
+    /**
+     * It provides a vertical or horizontal menu renderer depending on the current properties configuration.
+     * 
+     * @param menu The menu to be rendered.
+     * @return It returns the matching menu renderer.
+     */
+    protected MenuRenderer chooseRendererFor(Menu menu) {
+        return (Application.settings().isMenuLayoutHorizontal()) ? new HorizontalMenuRenderer(menu) : new VerticalMenuRenderer(menu);
+    }
+
+    /**
+     * It inserts a vertical separator if the menu is to be rendered vertically or nothing if horizontal.
+     * 
+     * @param menu The menu to be rendered.
+     */
+    protected void separatorFor(Menu menu) {
+        if (!Application.settings().isMenuLayoutHorizontal()) {
+            menu.add(new VerticalSeparator());
+        }
+    }
+
 }
